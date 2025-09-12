3c5ab6b01f480a7bee7958b15ca3a189b3e1679e
MCSY <24104152@usc.edu.ph>
2025-09-12 09:47:42 +0800

    Create Quiz 1 Answer.c

---
+++ Notes/Quiz/Quiz 1 Answer.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void display(int arr[], int n);
+
+int main() {
+    int size = 5;
+    int *arr = malloc(sizeof(int) * size);
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr[3] = 40;
+    arr[4] = 50;
+    display(arr, size);
+    
+    printf("Third Element: %d\n", *(arr + 2));
+    printf("Last Element: %d\n", arr[size - 1]);
+    
+    arr = realloc(arr, sizeof(int) * (size + 3));
+    arr[size++] = 60;
+    display(arr, size);
+    
+    int i;
+    for(i = size; i > 0; --i) {
+        if(arr[i - 1] > 25) {
+            arr[i] = arr[i - 1];
+        } else {
+            break;
+        }
+    }
+    arr[i] = 25;
+    size++;
+    display(arr, size);
+    
+    for(i = 0; i < size && arr[i] != 40; ++i) {}
+    for(; i < size; ++i) {
+        arr[i] = arr[i+1];
+    }
+    size--;
+    display(arr, size);
+    
+    arr[0] = 5;
+    display(arr, size);
+    
+    printf("SIZE: %d\n", sizeof(arr)/sizeof(int));
+    
+    printf("REVERSE: ");
+    for(int i = size - 1; i >= 0; --i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    for(i = 0; i < size && arr[i] != 25; ++i) {}
+    printf("INDEX: %d", i);
+    
+    return 0;
+}
+
+void display(int arr[], int n) {
+    printf("{");
+    for(int i = 0; i < n; ++i) {
+        printf("%d", arr[i]);
+        if(i < n - 1) {
+            printf(", ");
+        }
+    }
+    printf("}\n");
+}
