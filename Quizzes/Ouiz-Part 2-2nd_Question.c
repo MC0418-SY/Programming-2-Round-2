d5f9604410594102e479fb2e960ff041ba20ddea
MCSY <24104152@usc.edu.ph>
2025-09-12 11:12:51 +0800

    Update Ouiz-Part 2-2nd_Question.c

--- Quizzes/Ouiz-Part 2-2nd_Question.c
+++ Quizzes/Ouiz-Part 2-2nd_Question.c
@@ -1,1 +1,29 @@
+/*
+  2. Write a function 
+        filterEven()
+      that returns a new dynamically allocated array 
+      containing all the even values. This time no size, 
+      the array is appended with a sentinel (-1).
 
+      For example,
+        arr --> | 1 | 2 | 3 | 4 | 5 | -1 |
+        Index     0   1   2   3   4    5
+
+      In this example, there are 5 elements.
+      ----------------------------------------
+      Another example,
+        arr --> | -1 |   |   |   |   |   |
+        Index     0    1   2   3   4   5
+        
+      In this example, the array is empty.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+  
+  return 0;
+}
