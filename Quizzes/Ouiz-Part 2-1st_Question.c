df0f1dbf72a9b28d99cfcdd43d9572fecd0164e2
MCSY <24104152@usc.edu.ph>
2025-09-12 10:11:45 +0800

    Update Ouiz-Part 2-1st_Question.c
    This doesn't work

--- Quizzes/Ouiz-Part 2-1st_Question.c
+++ Quizzes/Ouiz-Part 2-1st_Question.c
@@ -1,1 +1,48 @@
+#include <stdio.h>  
+#include <stdlib.h> 
 
+
+int *filterEven(int nums[], int size, int *newSize);
+
+// Main Function
+int main() {
+    int size = 5;
+    int hayst;
+    int arr[] = {10, 20, 30, 40, 50};
+
+
+    int *omg = filterEven(arr, size, &hayst);
+    
+    printf("bruh");
+
+    return 0;
+}
+
+int *filterEven(int nums[], int size, int *newSize){
+    
+    int evenCount = 0;
+    
+    for (int i = 0; i < size; ++i){
+        if (nums[i] % 2 == 0){
+            (*newSize)++;
+        }
+    }
+    
+    int *newArr = (int*)malloc(size * sizeof(int));
+    
+    for(int i = 0; i < *newSize; ++i){
+        if (nums[i] % 2 == 0){
+            newArr[i] = nums[i];
+        }
+    }
+    
+    return newArr;
+    
+    
+    
+    
+    
+    
+    
+    
+}
