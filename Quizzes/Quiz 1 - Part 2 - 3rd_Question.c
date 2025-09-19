f03cbeb2256941b2c7008acce2865fbc7f423a4a
MCSY <24104152@usc.edu.ph>
2025-09-19 08:26:56 +0800

    Update Quiz 1 - Part 2 - 3rd_Question.c

--- Quizzes/Quiz 1 - Part 2 - 3rd_Question.c
+++ Quizzes/Quiz 1 - Part 2 - 3rd_Question.c
@@ -2,8 +2,6 @@
     3. Write a display function that uses the array property in question 2 i.e. the sentinel value.
         void display(int *arr);
         void display(int arr[]);
-
-        
-
-
 */
+
+
