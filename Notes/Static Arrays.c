f2e2ec63299887558cb0a9c3a657a18b375f21df
MCSY <24104152@usc.edu.ph>
2025-10-21 20:36:36 +0800

    Modify static array and add dynamic array example
    Updated static array elements and added dynamic array example.

--- Notes/Static Arrays.c
+++ Notes/Static Arrays.c
@@ -2,9 +2,36 @@
 #include <stdlib.h>
 
 int main(){
-  int arr[] = {10, 20, 30, 40, 50};
-
-  
-
-  return 0;
+    int size = 5;
+    
+    int arr[] = {11, 22, 32, 44, 55};
+    //This is how you set the elements in a STATIC array
+    
+    for(int i = 0; i < size; ++i) {
+        printf("%d", arr[i]);
+        if(i < size - 1) {
+            printf(", ");
+        }
+    }
+    
+    printf("\n");
+    
+    
+    int *newArr = (int*)malloc(sizeof(int) * size);
+    
+    //This is you set the elements in a DYNAMIC array
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr[3] = 40;
+    arr[4] = 50;
+    
+    for(int i = 0; i < size; ++i) {
+        printf("%d", arr[i]);
+        if(i < size - 1) {
+            printf(", ");
+        }
+    }
+    
+    return 0;
 }
