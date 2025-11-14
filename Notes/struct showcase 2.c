307838ea530bded258deb780266e786f876eafbb
MCSY <24104152@usc.edu.ph>
2025-11-14 09:01:11 +0800

    Add Point struct and functions for point display

---
+++ Notes/struct showcase 2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 50
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+
+Point newPoint(int x, int y);
+void displayPoint(Point p);
+// sir wants to display it like this
+// (x, y) -> (5, 7);
+
+int main(){
+    Point p1, p2;
+    
+    p1 = newPoint(5, -7);
+    
+    
+    p2.x = 15;
+    p2.y = 30;
+    
+    displayPoint(p1);
+    displayPoint(p2);
+    
+    return 0;
+}
+
+Point newPoint(int x, int y){
+    Point newPoint;
+    newPoint.x = x;
+    newPoint.y = y;
+    return newPoint;
+}
+
+void displayPoint(Point p){
+    printf("Coordinates: (%d, %d)", p.x, p.y);
+    printf("\n");
+}
